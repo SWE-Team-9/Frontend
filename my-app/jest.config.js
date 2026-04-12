9e6d8f668e46dbea80df65402bdd34d41fc240b7
Maryamsol37 <maryamsol004@gmail.com>
2026-04-12 20:17:47 +0200

    test: configure Jest for Next.js and testing-library

--- my-app/jest.config.js
+++ my-app/jest.config.js
@@ -1,12 +1,18 @@
-const nextJest = require('next/jest')
-const createJestConfig = nextJest({
-  dir: './',
-})
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-'^@/(.*)$': '<rootDir>/$1',  },
-}
-module.exports = createJestConfig(customJestConfig)
+const nextJest = require("next/jest");
 
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  clearMocks: true,
+  restoreMocks: true,
+};
+
+module.exports = createJestConfig(customJestConfig);
